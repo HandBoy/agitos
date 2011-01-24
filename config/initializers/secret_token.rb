2b894e3496ca3b79683d8134ce9796a753354a71
HandBoy <handersonmedeiros@gmail.com>
2011-01-23 23:18:27 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agitos::Application.config.secret_token = 'a4ed24c8a925aa285fd3ab6d7849453e6e9bdd15c2e2f83635d52cd8684de113842ca4115cd8c8c5229c2b2b1303d3ed9f029284ed4090e128e01790d20be9d8'
